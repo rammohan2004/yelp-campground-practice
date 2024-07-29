fc74dee0e01a241106d268eb88444592e7a83c97
Ram mohan <ramamohanraobotla12@gmail.com>
2024-07-29 11:30:52 +0530

    uploading images

--- seeds/index.js
+++ seeds/index.js
@@ -3,11 +3,7 @@ const cities = require('./cities');
 const { places, descriptors } = require('./seedHelpers');
 const Campground = require('../models/campground');
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp', {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true
-});
+mongoose.connect('mongodb://localhost:27017/yelp-camp');
 
 const db = mongoose.connection;
 
@@ -25,10 +21,19 @@ const seedDB = async () => {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
-            author: '5f5c330c2cd79d538f2c66d9',
+            author: '66a28ff5c1ae9ab9399c15df',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
-            image: 'https://source.unsplash.com/collection/483251',
+            images:[
+                {
+                  url: 'https://res.cloudinary.com/dle2qaskm/image/upload/v1722180316/yelpCamp/anau87yz8fuhveib5r9f.jpg',
+                  filename: 'yelpCamp/anau87yz8fuhveib5r9f',
+                },
+                {
+                  url: 'https://res.cloudinary.com/dle2qaskm/image/upload/v1722181494/yelpCamp/skuvaczo1e0lqsde4zav.jpg',
+                  filename: 'yelpCamp/skuvaczo1e0lqsde4zav',
+                }
+              ],
             description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!',
             price
         })

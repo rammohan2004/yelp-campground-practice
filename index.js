8200b7b9d82b64cffc5bd540526f0dc132dd328f
Ram mohan <ramamohanraobotla12@gmail.com>
2024-07-30 13:25:18 +0530

    maps

--- index.js
+++ index.js
@@ -18,7 +18,8 @@ const userRoutes = require('./routes/users');
 const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
 const MongoStore = require('connect-mongo')
-const dbUrl='mongodb://localhost:27017/yelp-camp';
+
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
 mongoose.connect(dbUrl);
 
 
